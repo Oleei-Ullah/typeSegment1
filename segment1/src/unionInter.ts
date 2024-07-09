e395d6eb77ee1861d94b99d86cf9d5dc4a809780
Oleei-Ullah <oleeiullah.coder.97@gmail.com>
2024-07-09 18:46:57 +0600

    union and intersection types explored

--- segment1/src/unionInter.ts
+++ segment1/src/unionInter.ts
@@ -4,26 +4,53 @@
 
     //Union types...
 
-    type TUser = {
-        name: string;
-        age: number;
-        gender: 'male' | 'female';   //string literal type as union types
-        codingSkill: 'high' | 'medium'
+    // type TUser = {
+    //     name: string;
+    //     age: number;
+    //     gender: 'male' | 'female';   //string literal type as union types
+    //     codingSkill: 'high' | 'medium'
+    // }
+
+    // const user: TUser = {
+    //     name: 'Khalid',
+    //     age: 20,
+    //     gender: 'female',
+    //     codingSkill: 'medium'
+    // }
+
+
+    // type Tamim = 'Dedicated' | 'Non-Dedicated';
+    // type Sakib = 'Chur' | 'Dandabaz';
+
+    // type Player = Tamim | Sakib; //union type used in type
+
+    // const aPlayer : Player = 'Chur'
+
+
+
+    //intersection types...
+
+    type Good = {
+        polite: string;
+        obedient : boolean;
+        loyal: boolean
     }
 
-    const user: TUser = {
-        name: 'Khalid',
-        age: 20,
-        gender: 'female',
-        codingSkill: 'medium'
+    type Better = {
+        inelligent: boolean,
+        courageous: boolean,
+        ellegent: 'Gained' | "pained"
     }
 
 
-    type Tamim = 'Dedicated' | 'Non-Dedicated';
-    type Sakib = 'Chur' | 'Dandabaz';
+    type Police = Good & Better;  //intersection type
 
-    type Player = Tamim | Sakib; //union type used in type
-
-    const aPlayer : Player = 'Chur'
-
+    const police: Police = {
+        polite: 'YES',
+        obedient: true,
+        loyal: true,
+        inelligent: false,
+        courageous: true,
+        ellegent: 'Gained'
+    }
 }
