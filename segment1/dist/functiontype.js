d99a0e45c582785a4f53b45d3cdf003fc5876098
Oleei-Ullah <oleeiullah.coder.97@gmail.com>
2024-07-09 12:33:52 +0600

    method in object with typescript explored:

--- segment1/dist/functiontype.js
+++ segment1/dist/functiontype.js
@@ -1,5 +1,17 @@
 "use strict";
-const add = (num1, num2) => {
-    return `${num1 + num2}`;
-};
-console.log(add(29, 2));
+{
+    const add = (num1, num2) => `${num1 + num2}`;
+    function normalAdd(num1, num2) {
+        return num1 * num2;
+    }
+    //Methods of a object with typescript here.....
+    const person = {
+        name: "khalid",
+        balance: 5900,
+        //normal anonymous function as 'this' keyword doesn't work on arrow function..
+        addBalance(balance) {
+            return `${this.balance * balance}`;
+        }
+    };
+    console.log(person.addBalance(5));
+}
--- segment1/src/functiontype.ts
+++ segment1/src/functiontype.ts
@@ -1,8 +1,32 @@
-const add = (num1: number, num2: number) : string => `${num1 + num2}`
+{
+
+    const add = (num1: number, num2: number) : string => `${num1 + num2}`
 
 function normalAdd (num1: number, num2: number): number {
     return num1 * num2
 }
 
 
-console.log(add(29, 2));
+
+//Methods of a object with typescript here.....
+
+
+const person = {
+    name: "khalid",
+    balance: 5900,
+    //normal anonymous function as 'this' keyword doesn't work on arrow function..
+    addBalance (balance: number) : string {
+        return `${this.balance * balance}`
+    }
+}
+
+
+console.log(person.addBalance(5));
+
+
+
+
+
+
+
+}
