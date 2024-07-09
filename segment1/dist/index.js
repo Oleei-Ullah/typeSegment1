2ab8a40d6532d5db68a08d1f3bf5568712cd3165
Oleei-Ullah <oleeiullah.coder.97@gmail.com>
2024-07-09 11:45:10 +0600

    array type and tupled type explored

--- segment1/dist/index.js
+++ segment1/dist/index.js
@@ -5,3 +5,8 @@ const undefinedtype = undefined;
 const nullish = null;
 let d;
 d = 34;
+const friends = ['abdullah', 'umar', 'khalid'];
+const borrowedMoney = [23, 34];
+const tupledArray = [23, 'khalid', true];
+tupledArray.push(4);
+console.log(tupledArray);
--- segment1/src/index.ts
+++ segment1/src/index.ts
@@ -9,3 +9,11 @@ const nullish : null = null;
 let d : number;
 
 d = 34;
+
+
+const friends : string[] = ['abdullah', 'umar', 'khalid'];
+
+const borrowedMoney : number[] = [23,34];
+
+
+const tupledArray : [number, string, boolean] = [23, 'khalid', true];
