9697aae7d1091806d8d62aa1587141781d9c5714
Oleei-Ullah <oleeiullah.coder.97@gmail.com>
2024-07-09 13:16:33 +0600

    destruction is beauty in typescript

---
+++ segment1/dist/destructuring.js
@@ -0,0 +1,18 @@
+"use strict";
+{
+    const khalifa = {
+        id: 1,
+        name: {
+            firstName: 'Khalid',
+            middleName: 'Bin',
+            lastName: 'Walid'
+        },
+        occupation: 'Fighter'
+    };
+    const { name: { middleName: midName } } = khalifa;
+    console.log(midName);
+    //array destructure....
+    const sahabaKeram = ['Ali', 'Umar', 'Bakar', 'Usman'];
+    const [, ...rest] = sahabaKeram;
+    console.log(rest);
+}
