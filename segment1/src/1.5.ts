c7592283e9796aed8d75ed0a04724e1e5024d79f
Oleei-Ullah <oleeiullah.coder.97@gmail.com>
2024-07-09 11:54:33 +0600

    literal type has been explored

--- segment1/src/1.5.ts
+++ segment1/src/1.5.ts
@@ -5,4 +5,16 @@ const person : {
 } = {
     firstName : 'Abdullah',
     lastName: "Amir Hamza"
+};
+
+
+
+const getFeature : {
+    feature : 'Video Calling',  //literal type --> when peroperties value act like the type its callded the literal type.
+    developingDuration: number,
+    isFinished: boolean
+} = {
+    feature: 'Video Calling',
+    developingDuration: 3,
+    isFinished: true
 }
